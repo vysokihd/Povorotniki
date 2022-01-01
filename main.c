b682619deaf4ad10fe4f0c759006735aedf7c962
Дмитрий Высоких <vysokihd@gmail.com>
2022-01-01 21:05:48 +0300

    Добавлено состояние MODE_PREOFF, при котором модуль ждёт полной неактивности реле для продолжения работы, что повышает стабильность и предотвращает возможные ложные срабатывания.

--- main.c
+++ main.c
@@ -24,23 +24,25 @@
 #define DIR_OFF(P)			PORTB |= (P)
 
 //------------------ Тайминги ---------------------------
-#define DELAY_OFF		50
+#define TIMEOUT			5000			//Таймаут выключения направления если что-то пошло не так, мс
+#define DELAY_SCAN		50				//Задержка перед сканированием направления, мс
+#define DELAY_OFF		250				//Задержка перед переходом в MODE_OFF, мс
 #define DELAY_ON		20				//Задержка включения направления, мс
-#define FLASHES_1		5				//Кол-во вспышек поворотников
-#define FLASHES_2		3				//Кол-во вспышек аварийкой
+#define FLASHES_1		5				//Кол-во вспышек поворотников (x+1)/2
+#define FLASHES_2		3				//Кол-во вспышек аварийкой	(x+1)/2
 
 typedef enum
 {
 	MODE_OFF,
 	MODE_LEFT,
 	MODE_RIGHT,
-	MODE_BOTH	
+	MODE_BOTH,
+	MODE_PREOFF,	
 }STATE;
 
 STATE state = MODE_OFF;				//Состояние логического автомата
-//volatile uint16_t tim0_tick = 0;	//Таймерная переменная
-//volatile bool timStart = false;		//Запуск таймерной переменной
 volatile uint8_t chgCntr = 0;		//Счётчик изменений состояния пина обратной связи
+volatile uint16_t tim_ms = 0;		//Время, мс
 
 static inline void mcuInit()
 {
@@ -53,27 +55,52 @@ static inline void mcuInit()
 	PCMSK = (0 << PCINT5) | (0 << PCINT4) | (1 << PCINT3) | (0 << PCINT2) | (0 << PCINT1) | (0 << PCINT0); //Прерывание по пину PCINT0..PCINT5
 	GIFR = (0 << INTF0) | (1 << PCIF);			//Регистр флагов прерываний INT0 и PCINT0..PCINT5, 1 - сброс флага перывания
 	GIMSK = (0 << INT0) | (1 << PCIE);			//Разрешение прерывание INT0, PCINT0..PCINT5
+	
+	//----------------- Инициализация таймера TC0 -------------------
+	TCNT0 = 0x00;									//Сброс счётного регистра
+	OCR0A = 126;									//Установка регистра сравнения A
+	OCR0B = 0x00;									//Сброс регистра сравнения B
+	TCCR0A = (0<<COM0A1) | (0<<COM0A0) |			//OC0A - отключён
+			 (0<<COM0B1) | (0<<COM0B0) |			//OC0B - отключён
+			 (1<<WGM01) | (0<<WGM00);				//Режим работы таймера (сброс при совпадении с OCR0A)
+	TCCR0B = (0<<WGM02) |							//Режим работы таймера
+			 (0<<CS02) | (1<<CS01) | (0<<CS00);		//Запуск таймера с делителем 8
+	TIMSK0 = (0<<OCIE0B) |							//Прерывание по сравнению с регистром сравнения B
+			 (1<<OCIE0A) |							//Прерывание по сравнению с регистром сравнения A
+			 (0<<TOIE0);							//Прерывание по переполнению
+}
+
+uint16_t msTime()
+{
+	cli();
+	uint16_t tm = tim_ms;
+	sei();
+	return tm;
+}
+void msTimeClr()
+{
+	cli();
+	tim_ms = 0;
+	sei();
 }
 
 int main(void)
 {
 	mcuInit();
 	bool changeDir = false;
-	uint16_t leftDir = 0;
-	uint16_t rightDir = 0;
-	uint16_t bothDir = 0;
-	
+	DIR_OFF(1 << LEFT_OUT | 1 << RIGHT_OUT);	
 	sei();
-    while (1) 
-    {
+
+	while (1) 
+	{
 		switch (state)
 		{
 		case MODE_OFF:
-			chgCntr = 0;
-			DIR_OFF(1 << LEFT_OUT | 1 << RIGHT_OUT);
-			//Ожидание активной работы модуля
+			//chgCntr = 0;
+			//DIR_OFF(1 << LEFT_OUT | 1 << RIGHT_OUT);
+			//Ожидание активной работы модуля (пока FB_IN==0 модуль не активен)
 			while(PIN_IS_CLER(1 << FB_IN));
-			_delay_ms(50);
+			_delay_ms(DELAY_SCAN);
 			//Проверка входящего сигнала налево
 			if(PIN_IS_SET(1 << LEFT_IN) && PIN_IS_CLER (1 << RIGHT_IN))	
 			{	
@@ -81,6 +108,9 @@ int main(void)
 				if(PIN_IS_SET(1 << LEFT_IN) && PIN_IS_CLER (1 << RIGHT_IN))
 				{
 					state = MODE_LEFT;
+					DIR_ON(1 << LEFT_OUT);
+					chgCntr = 0;
+					msTimeClr();
 				}
 			}
 			//Проверка входящего сигнала направо
@@ -90,73 +120,73 @@ int main(void)
 				if(PIN_IS_SET(1 << RIGHT_IN) && PIN_IS_CLER (1 << LEFT_IN))
 				{
 					state = MODE_RIGHT;
+					DIR_ON(1 << RIGHT_OUT);
+					chgCntr = 0;
+					msTimeClr();
 				}
 			}
 			//Проверка одновременного срабатывания налево и направо (аварийка)
-			if(PIN_IS_SET(1 << LEFT_IN) && PIN_IS_SET(1 << RIGHT_IN))
+			if(PIN_IS_SET(1<<LEFT_IN) && PIN_IS_SET(1<<RIGHT_IN))
 			{
 				_delay_ms(DELAY_ON);
-				if(PIN_IS_SET(1 << LEFT_IN) && PIN_IS_SET(1 << RIGHT_IN))
+				if(PIN_IS_SET(1<<LEFT_IN) && PIN_IS_SET(1<<RIGHT_IN))
 				{
 					state = MODE_BOTH;
+					DIR_ON(1 << RIGHT_OUT | 1 << LEFT_OUT);
+					chgCntr = 0;
+					msTimeClr();
 				}
 			}
 		break;
 		
 		case MODE_LEFT:
-			//Удержание направления налево			
-			DIR_ON(1 << LEFT_OUT);
-			changeDir = PIN_IS_SET(1 << FB_IN) && PIN_IS_SET(1 << RIGHT_IN);
-			_delay_ms(10);
-			changeDir &= PIN_IS_SET(1 << FB_IN) && PIN_IS_SET(1 << RIGHT_IN);
+			//Проверка смены направления			
+			changeDir = PIN_IS_SET(1<<FB_IN) && PIN_IS_SET(1<<RIGHT_IN);
+			_delay_ms(5);
+			changeDir &= PIN_IS_SET(1<<FB_IN) && PIN_IS_SET(1<<RIGHT_IN);
 			
-			//Выход по: лимит кол-ва вспышек налево | сигналу направо
-			if(chgCntr >= FLASHES_1 || changeDir)
+			//Выход по: лимит кол-ва вспышек налево | смене направления | таймауту
+			if(chgCntr >= FLASHES_1 || changeDir || msTime() > TIMEOUT)
 			{
-				DIR_OFF(1 << LEFT_OUT | 1 << RIGHT_OUT);
-				while(PIN_IS_SET (1 << LEFT_IN));
-				if(changeDir)
-				{
-					state = MODE_RIGHT;
-					chgCntr = 0;
-				}
-				else state = MODE_OFF;
-				//_delay_ms(DELAY_OFF);
+				state = MODE_PREOFF;
+				msTimeClr();
 			}
 		break;
 		
 		case MODE_RIGHT:
-			//Удержание направления направо
-			DIR_ON(1 << RIGHT_OUT);
-			changeDir = PIN_IS_SET(1 << FB_IN) && PIN_IS_SET(1 << LEFT_IN);
-			_delay_ms(10);
-			changeDir &= PIN_IS_SET(1 << FB_IN) && PIN_IS_SET(1 << LEFT_IN);
+			//Проверка смены направления
+			changeDir = PIN_IS_SET(1<<FB_IN) && PIN_IS_SET(1<<LEFT_IN);
+			_delay_ms(5);
+			changeDir &= PIN_IS_SET(1<<FB_IN) && PIN_IS_SET(1<<LEFT_IN);
 			
-			//Выход по: лимит кол-ва вспышек направо | сигналу налево | таймауту
-			if(chgCntr >= FLASHES_1 || changeDir) 
+			//Выход по: лимит кол-ва вспышек направо | смене напрвыления | таймауту
+			if(chgCntr >= FLASHES_1 || changeDir || msTime() > TIMEOUT) 
 			{
-				DIR_OFF(1 << RIGHT_OUT | 1 << LEFT_OUT);
-				while(PIN_IS_SET(1 << RIGHT_IN));
-				if(changeDir)
-				{
-					state = MODE_LEFT;
-					chgCntr = 0;
-				}
-				else state = MODE_OFF;
-				//_delay_ms(DELAY_OFF);
+				state = MODE_PREOFF;
+				msTimeClr();
 			}
 		break;	
 		
 		case MODE_BOTH:
-			DIR_ON(1 << RIGHT_OUT | 1 << LEFT_OUT);
 			//Выход по: лимиту кол-ва вспышек аварийки | таймауту
-			if(chgCntr >= FLASHES_2)
+			if(chgCntr >= FLASHES_2 || msTime() > TIMEOUT)
 			{
-				DIR_OFF(1 << RIGHT_OUT | 1 << LEFT_OUT);
-				while(PIN_IS_SET(1 << RIGHT_IN) || PIN_IS_SET (1 << LEFT_IN));
-				state = MODE_OFF;
-				//_delay_ms(DELAY_OFF);
+				state = MODE_PREOFF;
+				msTimeClr();
 			}
+		break;
+		
+		case MODE_PREOFF:
+			DIR_OFF(1 << LEFT_OUT | 1 << RIGHT_OUT);
+			//Ожидание полной неактивности модуля поворотников
+			while(msTime() < DELAY_OFF)
+			{
+				if(PIN_IS_SET(1<<LEFT_IN) || PIN_IS_SET(1<<RIGHT_IN) || PIN_IS_SET(1<<FB_IN))
+				{
+					msTimeClr();
+				}
+			}
+			state = MODE_OFF;
 		break;
 		}
 	}
@@ -165,17 +195,11 @@ int main(void)
 //Вектор прерывания таймера
 ISR (TIM0_COMPA_vect)
 {
-	//if(timStart) 
-	//{
-		//tim0_tick++;
-	//}
+	tim_ms++;
 }
 
 //Вектор прерывания по изменению состояния пина
 ISR (PCINT0_vect)
 {
-	if(state != MODE_OFF)
-	{
-		chgCntr++;
-	}
+	chgCntr++;
 }
